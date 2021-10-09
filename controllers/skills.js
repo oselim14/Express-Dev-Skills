0fc5deee0d9cfb25f53c5dca94d1deb1d14d82dc
Tavi Selim <oselim14@gmail.com>
2021-10-08 19:08:14 -0700

    delete when selected

--- controllers/skills.js
+++ controllers/skills.js
@@ -2,7 +2,12 @@ const Skill = require('../models/skill');
 
 module.exports = {
     index,
-    show
+    show, 
+    new: newSkill,
+    create,
+    delete: deleteSkill,
+    edit, 
+    update,
 };
 
 function index(req, res) {
@@ -15,4 +20,30 @@ function show(req, res) {
     res.render('skills/show', {
         skill: Skill.getOne(req.params.id),
     });
+}
+
+function newSkill(req, res){
+    res.render('skills/new');
+}
+
+function create(req, res){
+    console.log(req.body);
+    Skill.create(req.body);
+    res.redirect('/skills');
+}
+
+function deleteSkill(req, res){
+    Skill.deleteOne(req.params.id);
+    res.redirect('/skills');
+}
+
+function edit(req, res) {
+    const skill = Skill.getOne(req.params.id);
+    res.render('skills/edit', {skill});
+}
+
+function update(req, res) {
+    req.body.learned = !!req.body.learned;
+    Skill.update(req.params.id, req.body);
+    res.redirect(`/skills/${req.params.id}`);
 }
--- models/skill.js
+++ models/skill.js
@@ -1,14 +1,18 @@
 const skills = [
-    {skill: 'javascript', nickname: 'JS', learned: true, id: 123},
-    {skill: 'HTML', nickname: 'HTML', learned: true, id: 234},
-    {skill: 'CSS', nickname: 'CSS', learned: true, id: 345},
-    {skill: 'jQuery', nickname: 'jQuery', learned: true, id: 456},
-    {skill: 'NodeJS', nickname: 'NodeJS', learned: true, id: 567},
+    {skill: 'Javascript', nickname: 'JS', learned: 'Yes', id: 123},
+    {skill: 'HTML', nickname: 'HTML', learned: 'Yes', id: 234},
+    {skill: 'CSS', nickname: 'CSS', learned: 'Yes', id: 345},
+    {skill: 'jQuery', nickname: 'jQuery', learned: 'Yes', id: 456},
+    {skill: 'NodeJS', nickname: 'NodeJS', learned: 'Yes', id: 567},
 ];
 
 module.exports = {
     getAll, 
-    getOne
+    getOne,
+    create, 
+    deleteOne,
+    update,
+
 };
 
 function getAll() {
@@ -19,3 +23,21 @@ function getOne(id) {
     id = parseInt(id);
     return skills.find(skill => skill.id === id);
 }
+
+function create(skill) {
+    skill.id = Date.now() % 10000;
+    skill.learned = 'no';
+    skills.push(skill);
+}
+
+function deleteOne(id) {
+    const idx = skills.findIndex(skill => skill.id === parseInt(id));
+    skills.splice(idx, 1);
+}
+
+function update(id, updatedSkill) {
+    id = parseInt(id);
+    const skill = skills.find(skill => skill.id === id);
+    Object.assign(skill, updatedSkill);
+}
+
